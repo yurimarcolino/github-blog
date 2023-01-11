102d57d648939715574783a8901251e72dbfb649
yurimarcolino <yurifmarcolino@gmail.com>
2023-01-11 12:52:46 -0300

    linter

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -1,10 +1,10 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
-export const LayoutContainer = styled.div`
-  .container {
-    width: 100%;
-    max-width: 70rem;
-    margin-right: auto;
-    margin-left: auto;
-  }
-`;
+export const LayoutContainer = styled.div``
+
+export const defaultContainer = styled.div`
+  width: 100%;
+  max-width: 54rem;
+  margin-right: auto;
+  margin-left: auto;
+`
